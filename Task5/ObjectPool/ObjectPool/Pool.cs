e991c9f4f7c62e7c6d9451fc1e6fab226c5f2f06
Adinai Iuldasheva <s20218@pjwstk.edu.pl>
2021-11-08 08:50:46 +0100

    initial commit

---
+++ Task5/ObjectPool/ObjectPool/Pool.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectPool
+{
+    public class Pool
+    {
+       
+        private int _counter = 0;
+        private int _maxSize = 5;
+
+        private readonly List<Item> objectsAvailable = new List<Item>();
+        private readonly List<Item> objectsNonAvailable = new List<Item>();
+
+        public void SetMaxSize(int size)
+        {
+            _maxSize = size;
+        }
+
+        public Item GetObject()
+        {
+            if(objectsAvailable.Count > 0)
+            {
+                var obj = objectsAvailable.First();
+                objectsAvailable.RemoveAt(0);
+                objectsNonAvailable.Add(obj);
+                Console.WriteLine("Object pulled.");
+                _counter--;
+                return obj;
+            }
+            else
+            {
+                Item obj = new Item();
+                objectsNonAvailable.Add(obj);
+                _counter++;
+                Console.WriteLine("New object created and pulled.");
+                return obj;
+            }
+        }
+
+        public void ReturnObject(Item obj)
+        {
+            if (_counter < _maxSize)
+            {
+                objectsAvailable.Add(obj);
+                _counter++;
+                Console.WriteLine("Object returned.");
+            }
+            else
+            {
+                Console.WriteLine("Maximum number of objects has been reached.");
+            }
+        }
+    }
+}
---
+++ Task5/ObjectPool/ObjectPool/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ObjectPool
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var pool = new Pool();
+
+            pool.SetMaxSize(2);
+
+            var obj1 = pool.GetObject();
+            var obj2 = pool.GetObject();
+            var obj3 = pool.GetObject();
+
+            pool.ReturnObject(obj2);
+        }
+    }
+}
